a91dbc77066e449b68875891105fbc50fe1cbc6d
bairejavier1 <bairejavier1@gmail.com>
2026-03-16 13:03:17 -0400

    Updated UI/UX

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css' // Global styles applied to the entire app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
